d298a02f74d83d7960875442920619b11678f5f7
Gregoire Boiron <gregoire.boiron@gmail.com>
2020-12-02 20:42:26 +0100

    + Add day2 part2

--- src/AdventOfCode2020/PasswordPhilosophy/PasswordPhilosophy.cpp
+++ src/AdventOfCode2020/PasswordPhilosophy/PasswordPhilosophy.cpp
@@ -28,57 +28,105 @@ int CountOccurenceOfCharInPassword(string password, const char& charToFind)
     return count;
 }
 
-bool IsPasswordLegit(int minBound, int maxBound, int nbOccurences)
+bool IsPasswordLegitPart1(int minBound, int maxBound, int nbOccurences)
 {
     return minBound <= nbOccurences && nbOccurences <= maxBound;
 }
 
-bool ComputeIfPasswordLegit(string password)
+bool IsPasswordLegitPart2(int index1, int index2, string password, const char& charToFind)
+{
+    cout << password << endl;
+    cout << charToFind << endl;
+    cout << password[index1 - 1] << endl;
+    cout << password[index2 - 1] << endl;
+    return password[index1 - 1] == charToFind && password[index2 - 1] != charToFind;
+}
+
+void GetInputsFromPassword(string line, string* password, int* minBound, int* maxBound, string* charToFind)
 {
     string spaceDelimiter = " ";
+    int firstIndexOfDelimiter = line.find_first_of(spaceDelimiter);
+    string bornesPolicy = line.substr(0, firstIndexOfDelimiter);
 
-    int firstIndexOfDelimiter = password.find_first_of(spaceDelimiter);
-    string bornesPolicy = password.substr(0, firstIndexOfDelimiter);
-
-    int minBound = -1;
-    int maxBound = -1;
-    getBoundsPolicy(bornesPolicy, &minBound, &maxBound);
-
+    getBoundsPolicy(bornesPolicy, minBound, maxBound);
     //cout << minBound << endl;
     //cout << maxBound << endl;
 
-    string charToFind = password.substr(firstIndexOfDelimiter + 1, 1);
+    *charToFind = line.substr(firstIndexOfDelimiter + 1, 1);
     //cout << "char to find " << charToFind << endl;
 
-    int nbOccurences = CountOccurenceOfCharInPassword(password.substr(firstIndexOfDelimiter + 3), *charToFind.c_str());
+    *password = line.substr(firstIndexOfDelimiter + 4);
+}
 
-    return IsPasswordLegit(minBound, maxBound, nbOccurences);
+bool ComputeIfPasswordLegitPart1(string line)
+{
+    int minBound = -1;
+    int maxBound = -1;
+    string* password = new string;
+    string* charToFind = new string;
+    GetInputsFromPassword(line, password, &minBound, &maxBound, charToFind);
+
+    int nbOccurences = CountOccurenceOfCharInPassword(*password, *(*charToFind).c_str());
+
+    delete password;
+    delete charToFind;
+
+    return IsPasswordLegitPart1(minBound, maxBound, nbOccurences);
+}
+
+bool ComputeIfPasswordLegitPart2(string line)
+{
+    int minBound = -1;
+    int maxBound = -1;
+    string* password = new string;
+    string* charToFind = new string;
+    GetInputsFromPassword(line, password, &minBound, &maxBound, charToFind);
+
+    bool result = IsPasswordLegitPart2(minBound, maxBound, *password, *charToFind->c_str())
+        || IsPasswordLegitPart2(maxBound, minBound, *password, *charToFind->c_str());
+
+    delete password;
+    delete charToFind;
+
+    return result;
 }
 
 int main()
 {
-    // TEST
-    /*string test = "9-12 q: qqqxhnhdmqqqqjz";
-    if (ComputeIfPasswordLegit(test))
+    // TESTS
+    string test = "9-12 q: qqqxhnhdmqqqqjz";
+    if (ComputeIfPasswordLegitPart1(test))
     {
-        cout << "Password legit !" << endl;
-    }*/
-    // END TEST
+        cout << "Password legit for part 1 !" << endl;
+    }
 
-    cout << "Password Philosophy" << endl;
+    test = "1-3 a: abcde";
+    if (ComputeIfPasswordLegitPart2(test))
+    {
+        cout << "Password legit for part 2 !" << endl;
+    }
+    // END TESTS
 
-    int total = 0;
+    cout << "===== Password Philosophy" << endl;
 
     string* array = new string[1000];
     int arrayLength = Utils::CreateStringArrayFromInputFile("input.txt", array);
 
+    int nbPasswordLegitPart1 = 0;
+    int nbPasswordLegitPart2 = 0;
     for (size_t i = 0; i < arrayLength; i++)
     {
-        if (ComputeIfPasswordLegit(array[i]))
+        if (ComputeIfPasswordLegitPart1(array[i]))
         {
-            total++;
+            nbPasswordLegitPart1++;
+        }
+
+        if (ComputeIfPasswordLegitPart2(array[i]))
+        {
+            nbPasswordLegitPart2++;
         }
     }
 
-    cout << "Result is " << total << endl;
+    cout << "Result Part 1 is " << nbPasswordLegitPart1 << endl;
+    cout << "Result Part 2 is " << nbPasswordLegitPart2 << endl;
 }
