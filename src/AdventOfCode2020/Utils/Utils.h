1df64a517b323c8f2a380fa22006001889f0d7bb
Gregoire Boiron <gregoire.boiron@gmail.com>
2020-12-03 20:03:34 +0100

    = Turn "Utils" from console application to static library to compile correctly everything

--- src/AdventOfCode2020/Utils/Utils.h
+++ src/AdventOfCode2020/Utils/Utils.h
@@ -10,6 +10,5 @@ class Utils
 public:
 	static int CreateIntArrayFromInputFile(const char* fileName, int* intArray);
 	static int CreateStringArrayFromInputFile(const char* fileName, string* intArray);
-
 };
 
