c687a504e15a58037d9dcab1d4afc4e5ba7881f5
Gregoire Boiron <gregoire.boiron@gmail.com>
2020-12-08 22:15:07 +0100

    + Add day 8 part 1 and part 2 solutions in the same file (will both work soon :construction: )

--- src/AdventOfCode2020/Day8-HandheldHalting/Day8-HandheldHalting.cpp
+++ src/AdventOfCode2020/Day8-HandheldHalting/Day8-HandheldHalting.cpp
@@ -26,14 +26,14 @@ int GetArgumentFromInstruction(string instruction)
     return value;
 }
 
-int main()
+vector<string> ConstructInstructionsFromFile(string fileName)
 {
     vector<string> instructions = vector<string>();
 
     string line;
     ifstream  myfile;
 
-    myfile.open("input.txt");
+    myfile.open(fileName);
     if (myfile.is_open())
     {
         while (getline(myfile, line))
@@ -44,92 +44,163 @@ int main()
     }
     myfile.close();
 
+    return instructions;
+}
+
+bool Part1(vector<string> instructions, int* result, bool swapWhenIndexMet = false, int swapIndex = -1)
+{
     vector<int> visitedInstructionPointer = vector<int>();
 
     int accumulator = 0;
     int instructionPointer = 0;
+    int nbJmpAndNopMet = 0;
+
+    bool isLooping = true;
+    bool isInstructorPointeurInBounds = true;
+    int loopIndex = 0;
+    do
+    {
+        visitedInstructionPointer.push_back(instructionPointer);
+
+        //cout << instructionPointer << " And size of vector " << instructions.size() << endl;
+        string currentInstruction = instructions[instructionPointer];
+        int number = GetArgumentFromInstruction(currentInstruction);
+
+        if (swapWhenIndexMet && nbJmpAndNopMet == swapIndex)
+        {
+            string newInstruction = currentInstruction.substr(0, 3) == "nop" ? "jmp" : "nop";
+            instructions[instructionPointer].replace(0, 3, newInstruction);
+        }
+        //cout << "Instruction is " << currentInstruction << " number is " << number << endl;
+
+        if (currentInstruction.substr(0, 3) == "acc")
+        {
+            accumulator += number;
+            instructionPointer++;
+        }
+        else if (currentInstruction.substr(0, 3) == "jmp")
+        {
+            instructionPointer += number;
+        }
+        else if (currentInstruction.substr(0, 3) == "nop")
+        {
+            instructionPointer++;
+        }
+
+        loopIndex++;
+        isInstructorPointeurInBounds = instructionPointer >= 0 && instructionPointer < instructions.size();
+        isLooping = find(visitedInstructionPointer.begin(), visitedInstructionPointer.end(), instructionPointer) != visitedInstructionPointer.end();
+    } while (loopIndex < SAFE_GUARD && isInstructorPointeurInBounds && !isLooping);
+
+    *result = accumulator;
+
+    return instructionPointer >= instructions.size();
+}
+
+int Part2(vector<string> instructions)
+{
+    //vector<int> visitedInstructionPointer = vector<int>();
+
+    int accumulator = 0;
+    //int instructionPointer = 0;
     int nextJmpOrNopToChange = 0;
 
+    bool hasProgramFinished = false;
     int mainLoopIndex = 0;
-    while (mainLoopIndex < SAFE_GUARD && instructionPointer < instructions.size())
+    while (mainLoopIndex < SAFE_GUARD && !hasProgramFinished)
     {
         nextJmpOrNopToChange++;
 
         vector<string> tempInstructions = vector<string>(instructions);
 
         // reset everything
-        accumulator = 0;
-        instructionPointer = 0;
-        visitedInstructionPointer.clear();
+        //accumulator = 0;
+        //instructionPointer = 0;
+        //visitedInstructionPointer.clear();
 
-        int jmpOrNopMet = 1;
-        int loopIndex = 0;
-        while (loopIndex < SAFE_GUARD && instructionPointer < tempInstructions.size() &&
-            find(visitedInstructionPointer.begin(), visitedInstructionPointer.end(), instructionPointer) == visitedInstructionPointer.end())
-        {
-            visitedInstructionPointer.push_back(instructionPointer);
+        //int jmpOrNopMet = 1;
+        //int loopIndex = 0;
 
-            cout << instructionPointer << " And size of vector " << tempInstructions.size() << endl;
-            string currentInstruction = tempInstructions[instructionPointer];
-            int number = GetArgumentFromInstruction(currentInstruction);
+        hasProgramFinished = Part1(tempInstructions, &accumulator, true, nextJmpOrNopToChange);
 
-            cout << "Number is " << number << endl;
+        //while (loopIndex < SAFE_GUARD && instructionPointer < tempInstructions.size() &&
+        //    find(visitedInstructionPointer.begin(), visitedInstructionPointer.end(), instructionPointer) == visitedInstructionPointer.end())
+        //{
+        //    visitedInstructionPointer.push_back(instructionPointer);
 
-            cout << "Instruction is " << currentInstruction << " number is " << number << endl;
+        //    cout << instructionPointer << " And size of vector " << tempInstructions.size() << endl;
+        //    string currentInstruction = tempInstructions[instructionPointer];
+        //    int number = GetArgumentFromInstruction(currentInstruction);
 
-            if (currentInstruction.substr(0, 3) == "acc")
-            {
-                cout << "RESULT UPDATED " << accumulator << endl;
-                accumulator += number;
-                instructionPointer++;
-            }
-            else if (currentInstruction.substr(0, 3) == "jmp")
-            {
-                if (jmpOrNopMet == nextJmpOrNopToChange)
-                {
-                    cout << "CHANGE IT" << endl;
-                    tempInstructions[instructionPointer].replace(0, 3, "nop");
+        //    cout << "Number is " << number << endl;
 
-                    // Do a nop here
-                    instructionPointer++;
-                }
-                else
-                {
-                    instructionPointer += number;
-                }
-                jmpOrNopMet++;
-            }
-            else if (currentInstruction.substr(0, 3) == "nop")
-            {
-                if (jmpOrNopMet == nextJmpOrNopToChange)
-                {
-                    if (instructionPointer + number >= 0 && instructionPointer + number < instructions.size())
-                    {
-                        cout << "CHANGE IT" << endl;
-                        tempInstructions[instructionPointer].replace(0, 3, "jmp");
-                        // Do a jump here
-                        instructionPointer += number;
-                    }
-                    else
-                    {
-                        nextJmpOrNopToChange++;
-                    }
-                }
-                else
-                {
-                    instructionPointer++;
-                }
-                jmpOrNopMet++;
-            }
+        //    cout << "Instruction is " << currentInstruction << " number is " << number << endl;
 
-            loopIndex++;
-        }
+        //    if (currentInstruction.substr(0, 3) == "acc")
+        //    {
+        //        cout << "RESULT UPDATED " << accumulator << endl;
+        //        accumulator += number;
+        //        instructionPointer++;
+        //    }
+        //    else if (currentInstruction.substr(0, 3) == "jmp")
+        //    {
+        //        if (jmpOrNopMet == nextJmpOrNopToChange)
+        //        {
+        //            cout << "CHANGE IT" << endl;
+        //            tempInstructions[instructionPointer].replace(0, 3, "nop");
 
-        cout << "Main loop" << endl;
+        //            // Do a nop here
+        //            instructionPointer++;
+        //        }
+        //        else
+        //        {
+        //            instructionPointer += number;
+        //        }
+        //        jmpOrNopMet++;
+        //    }
+        //    else if (currentInstruction.substr(0, 3) == "nop")
+        //    {
+        //        if (jmpOrNopMet == nextJmpOrNopToChange)
+        //        {
+        //            if (instructionPointer + number >= 0 && instructionPointer + number < instructions.size())
+        //            {
+        //                cout << "CHANGE IT" << endl;
+        //                tempInstructions[instructionPointer].replace(0, 3, "jmp");
+        //                // Do a jump here
+        //                instructionPointer += number;
+        //            }
+        //            else
+        //            {
+        //                nextJmpOrNopToChange++;
+        //            }
+        //        }
+        //        else
+        //        {
+        //            instructionPointer++;
+        //        }
+        //        jmpOrNopMet++;
+        //    }
+
+        //    loopIndex++;
+        //}
+
+        //cout << "Main loop" << endl;
 
         mainLoopIndex++;
     }
 
+    return accumulator;
+}
 
-    cout << "Result is : " << accumulator << endl;
+int main()
+{
+    vector<string> instructions = ConstructInstructionsFromFile("input.txt");
+
+    int resultPart1 = 0;
+    Part1(instructions, &resultPart1);
+
+    //int resultPart2 = Part2(instructions);
+
+    cout << "Result for part 1 is : " << resultPart1 << endl;
+    //cout << "Result for part 2 is : " << resultPart2 << endl;
 }
