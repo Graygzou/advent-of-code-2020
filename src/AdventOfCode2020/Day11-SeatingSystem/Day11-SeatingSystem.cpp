bac346ca374fa5f226ebcddae0d6ae732443ed56
Gregoire Boiron <gregoire.boiron@gmail.com>
2020-12-12 00:47:26 +0100

    Add day 11 part 2 with a simple brut force solution. Could try something else?

--- src/AdventOfCode2020/Day11-SeatingSystem/Day11-SeatingSystem.cpp
+++ src/AdventOfCode2020/Day11-SeatingSystem/Day11-SeatingSystem.cpp
@@ -9,136 +9,157 @@
 
 using namespace std;
 
-void FirstTry(int nbSpotLeft, vector<vector<char>> seatLayout)
+bool Part1NeighborhoodComputation(vector<vector<char>> previousLayout, int i, int j, int xBound, int yBound, int *neighborsOccupiedTotal)
+{
+    bool allNeighborsEmpty = true;
+
+    int minXBounds = max(0, i - 1);
+    int maxXBounds = min(i + 1, xBound);
+
+    int minYBounds = max(0, j - 1);
+    int maxYBounds = min(j + 1, yBound);
+    //cout << "study neighbors" << endl;
+    for (int neighborJ = minYBounds; neighborJ <= maxYBounds; neighborJ++)
+    {
+        for (int neighborI = minXBounds; neighborI <= maxXBounds; neighborI++)
+        {
+            if (neighborJ != j || neighborI != i)
+            {
+                //cout << "(" << neighborI << "," << neighborJ << ")" << endl;
+                if (previousLayout[neighborJ][neighborI] == '#')
+                {
+                    //cout << "neigbor found ! " << seatLayout[neighborJ][neighborI];
+                    (*neighborsOccupiedTotal)++;
+                }
+
+                allNeighborsEmpty = allNeighborsEmpty && previousLayout[neighborJ][neighborI] != '#';
+            }
+        }
+    }
+
+    return allNeighborsEmpty;
+}
+
+bool Part2NeighborhoodComputation(vector<vector<char>> previousLayout, int i, int j, int xBound, int yBound, int* neighborsOccupiedTotal)
+{
+    bool allNeighborsEmpty = true;
+
+    int minXBounds = max(0, i - 1);
+    int maxXBounds = min(i + 1, xBound);
+
+    int minYBounds = max(0, j - 1);
+    int maxYBounds = min(j + 1, yBound);
+    //cout << "study neighbors" << endl;
+    for (int neighborJ = minYBounds; neighborJ <= maxYBounds; neighborJ++)
+    {
+        for (int neighborI = minXBounds; neighborI <= maxXBounds; neighborI++)
+        {
+            if (neighborJ != j || neighborI != i)
+            {
+                //cout << "Here" << endl;
+
+                bool found = false;
+                int currentI = neighborI;
+                int currentJ = neighborJ;
+                while (!found &&
+                    currentI >= 0 && currentI <= xBound &&
+                    currentJ >= 0 && currentJ <= yBound)
+                {
+                    // Find it
+                    found = previousLayout[currentJ][currentI] != '.';
+
+                    if (!found)
+                    {
+                        // Update
+                        currentI += (neighborI - i);
+                        currentJ += (neighborJ - j);
+                    }
+                }
+
+                if (found)
+                {
+                    //cout << "(" << currentI << "," << currentJ << ")" << endl;
+                    if (previousLayout[currentJ][currentI] == '#')
+                    {
+                        //cout << "neigbor found ! " << seatLayout[neighborJ][neighborI];
+                        (*neighborsOccupiedTotal)++;
+                    }
+
+                    allNeighborsEmpty = allNeighborsEmpty && previousLayout[currentJ][currentI] != '#';
+                }
+            }
+        }
+    }
+
+    return allNeighborsEmpty;
+}
+
+
+
+int Part1(vector<vector<char>> seatLayout, int nbSeatOccupiedBeforeEmpty)
 {
     int nbOccupiedSeats = 0;
+
     int nbLoop = 0;
-    while (nbSpotLeft > 0 && nbLoop < 25)
+    bool hasChanged = false;
+    do
     {
-        nbSpotLeft = 0;
+        hasChanged = false;
+        vector<vector<char>> previousLayout = seatLayout;
 
-        bool exploreLine = false;
-        int i = 0;
-        int j = 0;
-
-        int startJ = j;
-        int startI = i;
-
-        while (j < seatLayout.size() && i < seatLayout[0].size())
+        for (int j = 0; j < previousLayout.size(); j++)
         {
-            //cout << seatLayout[j][i];
-
-            if (j < seatLayout.size() && i < seatLayout[0].size() && seatLayout[j][i] != '.' && seatLayout[j][i] != 'V')
+            for (int i = 0; i < previousLayout[0].size(); i++)
             {
-                //cout << "(" << i << "," << j << ")";
-
-                // tag the seat has occupied
-                int neighborsCount = 0;
-                // Leave the seat empty
-                bool hasOccupiedNeighbors = false;
-                bool allNeighborsEmpty = true;
-
-                // Find neighbors
-                int minXBounds = max(0, j - 1);
-                int maxXBounds = min(j + 1, (int)seatLayout.size() - 1);
-
-                int minYBounds = max(0, i - 1);
-                int maxYBounds = min(i + 1, (int)seatLayout[0].size() - 1);
-                for (size_t neighborJ = minXBounds; neighborJ <= maxXBounds; neighborJ++)
+                if (previousLayout[j][i] != '.')
                 {
-                    for (size_t neighborI = minYBounds; neighborI <= maxYBounds; neighborI++)
-                    {
-                        if (neighborJ != j || neighborI != i)
-                        {
-                            //cout << " loooooop >> ";
-                            if (seatLayout[neighborJ][neighborI] != '.')
-                            {
-                                //cout << "neigbor found ! " << seatLayout[neighborJ][neighborI];
-                                neighborsCount++;
-                            }
 
-                            hasOccupiedNeighbors |= seatLayout[neighborJ][neighborI] == 'V';
-                            allNeighborsEmpty &= seatLayout[neighborJ][neighborI] != 'V' && seatLayout[neighborJ][neighborI] != '#';
-                        }
-                    }
-                }
+                    //cout << "(" << i << "," << j << ") => " << previousLayout[j][i] << endl;
 
-                if (j < seatLayout[0].size() && i < seatLayout.size())
-                {
-                    //cout << " Compute ... Neighbor count = " << neighborsCount;
-                    if (neighborsCount < 4)
+                    // tag the seat has occupied
+                    int neighborsOccupiedTotal = 0;
+                    // Leave the seat empty
+                    bool hasOccupiedNeighbors = false;
+
+                    // Find neighbors
+                    //bool allNeighborsEmpty = Part1NeighborhoodComputation(previousLayout, i, j, (int)previousLayout[0].size() - 1, (int)previousLayout.size() - 1, &neighborsOccupiedTotal);
+                    bool allNeighborsEmpty = Part2NeighborhoodComputation(previousLayout, i, j, (int)seatLayout[0].size() - 1, (int)seatLayout.size() - 1, &neighborsOccupiedTotal);
+
+                    if (previousLayout[j][i] == 'L' && allNeighborsEmpty)
                     {
-                        //cout << " SCORE ! ";
-                        nbOccupiedSeats++;
-                        seatLayout[j][i] = 'V';
+                        hasChanged = true;
+                        seatLayout[j][i] = '#';
                     }
-                    else if (hasOccupiedNeighbors)
+
+                    if (previousLayout[j][i] == '#' && neighborsOccupiedTotal >= nbSeatOccupiedBeforeEmpty)
                     {
+                        hasChanged = true;
                         seatLayout[j][i] = 'L';
                     }
-                    else
-                    {
-                        // Apply regular rules
-                        nbSpotLeft++;
-                        if (seatLayout[j][i] == '?' && allNeighborsEmpty)
-                        {
-                            seatLayout[j][i] = '#';
-                        }
-                        else if (seatLayout[j][i] == '#' && neighborsCount >= 4)
-                        {
-                            seatLayout[j][i] = '?';
-                        }
-                    }
                 }
             }
-
-            if (exploreLine)
-            {
-                i++;
-                if (i >= seatLayout[0].size())
-                {
-                    exploreLine = false;
-                    startJ++;
-                    i = startI;
-                    j = startJ;
-
-                    //cout << endl;
-                }
-            }
-            else
-            {
-                j++;
-                if (j >= seatLayout.size())
-                {
-                    startI++;
-                    i = startI;
-                    j = startJ;
-
-                    //cout << endl;
-                }
-            }
-
-            /* cout << "starting I " << startI << " starting J " << startJ << endl;
-             cout << " I " << i << " J " << j << endl;*/
-             //cout << endl;
         }
-
-        // DEBUG: Display
-        cout << "Display current" << endl;
-        for (size_t i = 0; i < seatLayout.size(); i++)
-        {
-            for (size_t j = 0; j < seatLayout[i].size(); j++)
-            {
-                cout << seatLayout[i][j];
-            }
-
-            cout << endl;
-        }
-        cout << "end" << endl;
-        // END DEBUG
-
         nbLoop++;
-    }
+
+        //// DEBUG: Display
+        //cout << "Display current" << endl;
+        //for (size_t i = 0; i < seatLayout.size(); i++)
+        //{
+        //    for (size_t j = 0; j < seatLayout[i].size(); j++)
+        //    {
+        //        if (seatLayout[i][j] == '#')
+        //        {
+        //            nbOccupiedSeats++;
+        //        }
+        //        cout << seatLayout[i][j];
+        //    }
+
+        //    cout << endl;
+        //}
+
+
+    } while (hasChanged && nbLoop < 500);
+    cout << nbLoop << endl;
 
 
     // DEBUG: Display
@@ -147,13 +168,17 @@ void FirstTry(int nbSpotLeft, vector<vector<char>> seatLayout)
     {
         for (size_t j = 0; j < seatLayout[i].size(); j++)
         {
+            if (seatLayout[i][j] == '#')
+            {
+                nbOccupiedSeats++;
+            }
             cout << seatLayout[i][j];
         }
 
         cout << endl;
     }
-    cout << "end" << endl;
-    // END DEBUG
+
+    return nbOccupiedSeats;
 }
 
 
@@ -201,93 +226,9 @@ int main()
     cout << "TEST 1 " << seatLayout.size() << endl;
     cout << "TEST 2 " << seatLayout[0].size() << endl;
 
-    int nbOccupiedSeats = 0;
+    // Part1(seatLayout, 4)
+    int nbOccupiedSeats = Part1(seatLayout, 5);
 
-    int nbLoop = 0;
-    bool hasChanged = false;
-    do
-    {
-        hasChanged = false;
-        vector<vector<char>> previousLayout = seatLayout;
-
-        for (int j = 0; j < previousLayout.size(); j++)
-        {
-            for (int i = 0; i < previousLayout[0].size(); i++)
-            {
-                if (previousLayout[j][i] != '.')
-                {
-
-                    //cout << "(" << i << "," << j << ") => " << previousLayout[j][i] << endl;
-
-                    // tag the seat has occupied
-                    int neighborsOccupiedTotal = 0;
-                    // Leave the seat empty
-                    bool hasOccupiedNeighbors = false;
-                    bool allNeighborsEmpty = true;
-
-                    // Find neighbors
-                    int minXBounds = max(0, i - 1);
-                    int maxXBounds = min(i + 1, (int)seatLayout[0].size() - 1);
-
-                    int minYBounds = max(0, j - 1);
-                    int maxYBounds = min(j + 1, (int)seatLayout.size() - 1);
-                    //cout << "study neighbors" << endl;
-                    for (int neighborJ = minYBounds; neighborJ <= maxYBounds; neighborJ++)
-                    {
-                        for (int neighborI = minXBounds; neighborI <= maxXBounds; neighborI++)
-                        {
-                            if (neighborJ != j || neighborI != i)
-                            {
-                                //cout << "(" << neighborI << "," << neighborJ << ")" << endl;
-                                if (previousLayout[neighborJ][neighborI] == '#')
-                                {
-                                    //cout << "neigbor found ! " << seatLayout[neighborJ][neighborI];
-                                    neighborsOccupiedTotal++;
-                                }
-
-                                allNeighborsEmpty = allNeighborsEmpty && previousLayout[neighborJ][neighborI] != '#';
-                            }
-                        }
-                    }
-
-                    // COmpute neighbors
-
-                    if (previousLayout[j][i] == 'L' && allNeighborsEmpty)
-                    {
-                        hasChanged = true;
-                        seatLayout[j][i] = '#';
-                    }
-
-                    if (previousLayout[j][i] == '#' && neighborsOccupiedTotal >= 4)
-                    {
-                        hasChanged = true;
-                        seatLayout[j][i] = 'L';
-                    }
-                }
-            }
-        }
-        nbLoop++;
-
-        
-
-    } while (hasChanged && nbLoop < 100000);
-    cout << nbLoop << endl;
-
-
-    // DEBUG: Display
-    cout << "Display current" << endl;
-    for (size_t i = 0; i < seatLayout.size(); i++)
-    {
-        for (size_t j = 0; j < seatLayout[i].size(); j++)
-        {
-            if (seatLayout[i][j] == '#')
-            {
-                nbOccupiedSeats++;
-            }
-            //cout << seatLayout[i][j];
-        }
-    }
- 
     
     cout << "Result is : " << nbOccupiedSeats << endl;
 }
