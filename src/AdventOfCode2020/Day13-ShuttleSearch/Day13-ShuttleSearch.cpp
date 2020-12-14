42a2f966a5ebc968ad7c586917f14705d80fcdb0
Gregoire Boiron <gregoire.boiron@gmail.com>
2020-12-14 21:14:15 +0100

    + add day 13 part2 brut force but can't find a solution yet.

--- src/AdventOfCode2020/Day13-ShuttleSearch/Day13-ShuttleSearch.cpp
+++ src/AdventOfCode2020/Day13-ShuttleSearch/Day13-ShuttleSearch.cpp
@@ -60,40 +60,134 @@ int FindEarlistedAirportBus(string busSchedule, int earlistDepartPossible, int *
     return busID;
 }
 
-int FindEarliestTimestampForDepartureOffset(string busSchedule)
+long long FindEarliestTimestampForDepartureOffset(string busSchedule)
 {
-    vector<int> times;
+    int timestampFound = 0;
 
+    // This create a simple group since all numbers are prime in inputs.
+    vector<int> busIDs;
+    vector<int> offsets;
+    int maxIDFound = 0;
+    int maxIDIndex = 0;
+
+    int currentIndex(0);
+    int startingIndex(0);
+    string delimiter(",");
+    while (busSchedule.size() > 0)
+    {
+        string busTime("");
+        int nextDelimiterIndex = busSchedule.find(delimiter);
+
+        if (nextDelimiterIndex != string::npos)
+        {
+            busTime = busSchedule.substr(0, nextDelimiterIndex);
+        }
+        else
+        {
+            busTime = busSchedule;
+            nextDelimiterIndex = busSchedule.size() - 1;
+        }
+
+        if (busTime != "x")
+        {
+            int currentBusID = atoi(busTime.c_str());
+            busIDs.push_back(currentBusID);
+            offsets.push_back(currentIndex);
+
+            if (maxIDFound < currentBusID)
+            {
+                maxIDIndex = currentIndex;
+                maxIDFound = currentBusID;
+            }
+        }
+
+        currentIndex++;
+
+        // Update evertyhing
+        busSchedule = busSchedule.substr(nextDelimiterIndex + 1, busSchedule.size() - 1);
+        cout << busSchedule << endl;
+
+        startingIndex = nextDelimiterIndex++;
+        nextDelimiterIndex = busSchedule.find(delimiter);
+    }
+
+    /*for (size_t i = 0; i < busIDs.size(); i++)
+    {
+        cout << busIDs[i] << " / ";
+        cout << offsets[i] << ", ";
+    }
+    cout << endl;*/
+
+    cout << maxIDFound << endl;
+    cout << maxIDIndex << endl;
+
+
+    long long loopCount = 0;
+    bool found = false;
+
+    long long currentNumber = 0;
+    while (loopCount < 10000000000 && !found /*currentNumber < 1068797*/)
+    {
+        found = true;
+        for (int i = 0; found && i < busIDs.size(); i++)
+        {
+            //cout << currentNumber % busIDs[i] << " VS " << abs(i - maxIDIndex) << endl;
+
+            int currentBusOffset = currentNumber % busIDs[i];
+            int absIndexGap = abs(offsets[i] - maxIDIndex);
+
+            if (i <= maxIDIndex && currentBusOffset != absIndexGap ||
+                i > maxIDIndex && currentBusOffset != busIDs[i] - absIndexGap)
+            {
+                found = false;
+                currentNumber += maxIDFound;
+            }
+        }
+
+        //cout << currentNumber << endl;
+        loopCount++;
+    }
+    
+    if (!found)
+    {
+        cout << currentNumber << endl;
+        return -1;
+    }
+
+    cout << "RESULTTTTTT " << currentNumber << endl;
+    timestampFound = currentNumber - maxIDIndex;
+    return timestampFound;
 }
 
 int main()
 {
     std::cout << "Shuttle Search" << endl;
     int resultPart1(0);
+    long long resultPart2(0);
 
     ifstream file;
-    file.open("input.txt");
+    file.open("example1.txt");
     if (file.is_open())
     {
         int earliestTimestamp = -1;
 
-        int index = 0;
+        int index = 1;
         string line("");
         while (getline(file, line))
         {
-            if (earliestTimestamp == -1)
+            if (index == 0)
             {
                 earliestTimestamp = atoi(line.c_str());
                 cout << earliestTimestamp << endl;
             }
             else
             {
-                int waitingTime = 0;
-                int earliestBusID = FindEarlistedAirportBus(line, earliestTimestamp, &waitingTime);
+                //int waitingTime = 0;
+                //int earliestBusID = FindEarlistedAirportBus(line, earliestTimestamp, &waitingTime);
 
-                int resultPart2 = FindEarliestTimestampForDepartureOffset(line);
+                resultPart2 = FindEarliestTimestampForDepartureOffset(line);
 
-                resultPart1 = earliestBusID * waitingTime;
+                //resultPart1 = earliestBusID * waitingTime;
             }
             //cout << "POSITION IS NOW " << positionPart2.first << ", " << positionPart2.second << endl;
             //cout << "DIRECTION IS NOW " << waypoint.first << ", " << waypoint.second << endl;
@@ -101,4 +195,5 @@ int main()
     }
 
     std::cout << "Part 1 result " << resultPart1 << endl;
+    std::cout << "Part 2 result " << resultPart2 << endl;
 }
