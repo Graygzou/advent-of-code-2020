b8ff0aa09bd78e7e3e7cd712d7dbdb1a2d8883fb
Gregoire Boiron <gregoire.boiron@gmail.com>
2020-12-28 01:28:51 +0100

    + Optimize Day 23 part 2 to compute the result faster (not enough actually)

--- src/AdventOfCode2020/Day23-CrabCups/Day23-CrabCups.cpp
+++ src/AdventOfCode2020/Day23-CrabCups/Day23-CrabCups.cpp
@@ -38,104 +38,70 @@ void Part1(string fileName, bool part2)
     if (part2)
     {
         int nbCups = 1000000;
-        maxValue = nbCups;
-        for (size_t i = cups.size(); i <= nbCups; i++)
+        maxValue = nbCups - 1;
+        for (size_t i = cups.size(); i < nbCups; i++)
         {
             cups.push_back(i);
         }
     }
 
     cout << "Size " << cups.size() << endl;
-
-    int nbTurns = 10000000;
-    vector<int> previousPickUps;
-
-    int currentIndex = 0;
+    
     int pickUpNumber = 3;
+
+    vector<int> pickUps;
+    int currentIndex = 0;
+    int nbTurns = 10000000;
     for (size_t turn = 0; turn < nbTurns; turn++)
     {
-        int value = cups[currentIndex] - 1;
-        int lastIndex = min((int)cups.size(), currentIndex + 1 + pickUpNumber);
-        vector<int> pickUps = vector<int>(cups.begin() + currentIndex + 1, cups.begin() + lastIndex);
-        // Add if any element left need to be added.
-        if (lastIndex == cups.size())
-        {
-            for (int i = 0; i < (currentIndex + 1 + pickUpNumber) - cups.size(); i++)
-            {
-                pickUps.push_back(cups[i]);
-            }
-        }
-
         if ((turn + 1) % 1000 == 0)
-        {
             cout << "-- Move " << (turn + 1) << " --" << endl;
-        }
-       
+
         //cout << "cups: ";
         //for (size_t i = 0; i < cups.size(); i++)
         //{
-        //    if (i == currentIndex)
-        //    {
-        //        cout << "(";
-        //    }
-        //    cout << cups[i];
-        //    if (i == currentIndex)
-        //    {
-        //        cout << ")";
-        //    }
-        //    cout << ", ";
+        //    cout << cups[i] << ", ";
         //}
-        //cout << endl << "pick up: ";
-        //for (auto it = pickUps.begin(); it != pickUps.end(); ++it)
-        //{
-        //    cout << *it << ", ";
-        //}
-        
+        //cout << endl;
 
-        // remove extra elements at the beginning
-        int removingIndexStart = max(0, (currentIndex + pickUpNumber) - (int)cups.size());
-        cups.erase(cups.begin() + currentIndex + 1, cups.begin() + lastIndex);
+        int value = cups[currentIndex] - 1;
+        int lastIndex = min((int)cups.size(), currentIndex + 1 + pickUpNumber);
 
-        cups.erase(cups.begin(), cups.begin() + pickUpNumber - (lastIndex - (currentIndex + 1)));
-        
-        int destination = value;
-        
-        while (destination == 0 || 
-            find(pickUps.begin(), pickUps.end(), destination) != pickUps.end())
+        pickUps.clear();
+        int index = -1;
+        for (size_t i = 0; i < pickUpNumber; i++)
         {
-            if (destination == 0)
+            index = (currentIndex + 1) % cups.size();
+            if (index < currentIndex)
             {
-                destination = maxValue;
+                currentIndex--;
             }
-            else
-            {
-                destination--;
-            }
+
+            pickUps.push_back(cups[index]);
+            cups.erase(cups.begin() + index);
+        }
+
+        //cout << "cups: ";
+        //for (size_t i = 0; i < pickUps.size(); i++)
+        //{
+        //    cout << pickUps[i] << ", ";
+        //}
+        //cout << endl;
+
+        int destination = value;
+        while (destination == 0 || find(pickUps.begin(), pickUps.end(), destination) != pickUps.end())
+        {
+            destination = destination == 0 ? maxValue : destination - 1;
         }
 
         auto it = find(cups.begin(), cups.end(), destination);
-        int index = std::distance(cups.begin(), it);
-
-        //cout << endl << "destination: " << destination << endl;
-        //cout << endl << "index: " << index << endl;
-
-        //for (size_t i = 0; i < pickUps.size(); i++)
-        //{
-        //    cout << pickUps[i] << endl;
-        //}
-
-        if (index < currentIndex )
-        {
-            currentIndex = (currentIndex + pickUpNumber - (pickUpNumber - (lastIndex - (currentIndex + 1)))) % maxValue;
-        }
-
+        int destinationIndex = std::distance(cups.begin(), it);
         for(auto it = pickUps.rbegin(); it != pickUps.rend(); ++it)
         {
-            cups.insert(cups.begin() + index + 1, *it);
+            cups.insert(cups.begin() + destinationIndex + 1, *it);
         }
 
-        previousPickUps = pickUps;
-        currentIndex = (currentIndex + 1) % maxValue;
+        currentIndex = ((destinationIndex < currentIndex ? (currentIndex + pickUpNumber) % maxValue : currentIndex) + 1) % maxValue;
     }
 
     if (!part2)
