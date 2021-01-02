7820359cd3142178f06d6bfbed9f698e8f6cd678
Gregoire Boiron <gregoire.boiron@gmail.com>
2021-01-02 18:49:55 +0100

    + Add day 13 part 2 by using the chinese remainder theorem and the extended euclidean algorithm.

--- src/AdventOfCode2020/Day13-ShuttleSearch/Day13-ShuttleSearch.cpp
+++ src/AdventOfCode2020/Day13-ShuttleSearch/Day13-ShuttleSearch.cpp
@@ -159,38 +159,189 @@ long long FindEarliestTimestampForDepartureOffset(string busSchedule)
     return timestampFound;
 }
 
+void ExtractDataFromLine(string busSchedule, vector<int> *busIDs, vector<int> *offsets)
+{
+    int currentIndex(0);
+    string delimiter(",");
+    while (busSchedule.size() > 0)
+    {
+        string busTime("");
+        int nextDelimiterIndex = busSchedule.find(delimiter);
+
+        if (nextDelimiterIndex != string::npos)
+        {
+            busTime = busSchedule.substr(0, nextDelimiterIndex);
+        }
+        else
+        {
+            busTime = busSchedule;
+            nextDelimiterIndex = busSchedule.size() - 1;
+        }
+
+        if (busTime != "x")
+        {
+            int currentBusID = atoi(busTime.c_str());
+            busIDs->push_back(currentBusID);
+            offsets->push_back(currentIndex);
+        }
+
+        currentIndex++;
+
+        // Update evertyhing
+        busSchedule = busSchedule.substr(nextDelimiterIndex + 1, busSchedule.size() - 1);
+        cout << busSchedule << endl;
+
+        //startingIndex = nextDelimiterIndex++;
+        nextDelimiterIndex = busSchedule.find(delimiter);
+    }
+}
+
+unsigned long long inverse(unsigned long long number, int mod)
+{
+    signed long long t = 0;
+    signed long long newt = 1;
+    signed long long r = mod;
+    signed long long newr = number;
+    
+    while (newr != 0)
+    {
+        signed long long quotient = r / newr;
+
+        // Update left bezout coef
+        signed long long temp = t;
+        t = newt;
+        newt = temp - quotient * newt;
+
+        // Update remainders
+        temp = r;
+        r = newr;
+        newr = temp - quotient * newr;
+
+
+
+        // Update right bezout coef
+        //temp = previousRightBezoutCoef;
+        //previousRightBezoutCoef = rightBezoutCoef;
+        //rightBezoutCoef = temp - quotient * rightBezoutCoef;
+    }
+
+    if (r > 1)
+    {
+        cout << "number " << number << " is not inversible by mod " << mod << endl;
+        return 0;
+    }
+
+    // We always want a positive answer.
+    if (t < 0)
+    {
+        t += mod;
+    }
+
+    return t;
+
+    //cout << "Bezout coefficients:" << leftBezoutCoef << "," << previousRightBezoutCoef << endl;
+    //cout << "greatest common divisor:" << previousRemainder << endl;
+    //cout << "quotients by the gcd:" << rightBezoutCoef << "," << leftBezoutCoef << endl;
+
+    //return rightBezoutCoef;
+}
+
+unsigned long long ComputePart2(string busSchedule)
+{
+    vector<int> busIDs/*{3, 5, 7}*/;
+    vector<int> offsets/*{2,3,2}*/;
+    ExtractDataFromLine(busSchedule, &busIDs, &offsets);
+
+    cout << "Offsets " << endl;
+    for (size_t i = 0; i < offsets.size(); i++)
+    {
+        cout << offsets[i] << endl;
+    }
+    cout << endl;
+
+    cout << "Bus IDS" << endl;
+
+    // Apply the Chinese remainder theorem
+    // See https://en.wikipedia.org/wiki/Chinese_remainder_theorem
+    unsigned long long productOfMod = 1;
+    for (size_t i = 0; i < busIDs.size(); i++)
+    {
+        cout << busIDs[i] << endl;
+        productOfMod *= busIDs[i];
+    }
+    cout << "sumOfMod: " << productOfMod << endl;
+
+    unsigned long long result = 0;
+    for (size_t i = 0; i < busIDs.size(); i++)
+    {
+        unsigned long long n1 = busIDs[i];
+        unsigned long long n1hat = productOfMod / busIDs[i];
+        unsigned long long currentE = inverse(n1hat, n1) * n1hat;
+
+        int rangedOffset = offsets[i];
+        if (rangedOffset > n1)
+        {
+            rangedOffset = offsets[i] % n1;
+        }
+
+        unsigned long long realOffset = rangedOffset == 0 ? rangedOffset : n1 - rangedOffset;
+
+        cout << "n1: " << n1 << endl;
+        cout << "n1hat: " << n1hat << endl;
+        cout << "inverse: " << inverse(n1hat, n1) << endl;
+        cout << "offsets: " << realOffset << endl;
+        cout << "currentE: " << currentE << endl;
+        cout << realOffset * currentE << endl;
+        result += static_cast<unsigned long long>(realOffset * currentE);
+        cout << "result" << result << endl;
+    }
+
+    result %= productOfMod;
+
+    // Check if it's true
+    cout << "==========================" << endl;
+    for (size_t i = 0; i < busIDs.size(); i++)
+    {
+        cout << "Bus ID " << busIDs[i] << endl;
+        cout << "result is " << result % busIDs[i] << endl;
+        cout << "offset is " << busIDs[i] - offsets[i] << endl;
+    }
+    cout << "==========================" << endl;
+
+    // We want the smallest answer so apply the modulo directly.
+    return result;
+}
+
 int main()
 {
+    string fileName = "input.txt";
     std::cout << "Shuttle Search" << endl;
     int resultPart1(0);
-    long long resultPart2(0);
+    unsigned long long resultPart2(0);
 
     ifstream file;
-    file.open("example1.txt");
+    file.open(fileName);
     if (file.is_open())
     {
         int earliestTimestamp = -1;
 
-        int index = 1;
+        int index = 0;
         string line("");
         while (getline(file, line))
         {
             if (index == 0)
             {
                 earliestTimestamp = atoi(line.c_str());
-                cout << earliestTimestamp << endl;
+                index++;
             }
             else
             {
                 //int waitingTime = 0;
                 //int earliestBusID = FindEarlistedAirportBus(line, earliestTimestamp, &waitingTime);
-
-                resultPart2 = FindEarliestTimestampForDepartureOffset(line);
-
                 //resultPart1 = earliestBusID * waitingTime;
+
+                resultPart2 = ComputePart2(line);
             }
-            //cout << "POSITION IS NOW " << positionPart2.first << ", " << positionPart2.second << endl;
-            //cout << "DIRECTION IS NOW " << waypoint.first << ", " << waypoint.second << endl;
         }
     }
 
