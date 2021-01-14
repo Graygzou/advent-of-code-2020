db3dff2655185cda9ff32174da6eaa7f4b8c4733
Gregoire Boiron <gregoire.boiron@gmail.com>
2021-01-14 21:52:37 +0100

    = Clean day 10 with intermediate functions and more readable names

--- src/AdventOfCode2020/Day10-AdapterArray/Day10-AdapterArray.cpp
+++ src/AdventOfCode2020/Day10-AdapterArray/Day10-AdapterArray.cpp
@@ -6,227 +6,132 @@
 #include <fstream>
 #include <regex>
 #include <vector>
+#include <map>
+#include <stack>
 
 using namespace std;
 
-int ComputeFollowingGroup(vector<int> followingNumbers, int maxNumber)
+void DisplayVoltageRating(vector<int> voltageRating)
 {
-    int result = 0;
-
-    int i = 0;
-    vector<int> temp = vector<int>();
-    vector<int> save = vector<int>();
-
-    // DEBUG: Display
-    cout << "Display vector" << endl;
-    for (size_t i = 0; i < followingNumbers.size(); i++)
+    cout << "Display vector - Size = " << voltageRating.size() << endl;
+    for (size_t i = 0; i < voltageRating.size(); i++)
     {
-        cout << followingNumbers[i] << endl;
+        cout << voltageRating[i] << ", ";
     }
-    cout << "end" << endl;
-    cout << "max number is " << maxNumber << endl;
-    // END DEBUG
-    cout << "Length of vector is " << followingNumbers.size() << endl;
+    cout << endl;
+}
 
-    int currentNumber = 1;
+/// <summary>
+/// /!\ WARNING: This function required following numbers to be sorted.
+/// </summary>
+int ComputeFollowingGroup(vector<int> arrangementSegment, int maxNumber)
+{
+    int nbDistinctArrangementsForSegment = 0;
 
-    temp.push_back(followingNumbers[0]);
-    save.push_back(followingNumbers[0]);
+    const int minVoltageDiff = 1;
+    const int maxVoltageDiff = 3;
 
-    int globalIndex = 0;
-    while (!temp.empty())
+    stack<int> chargersLeft = stack<int>();
+    chargersLeft.push(arrangementSegment[0]);
+
+    while (!chargersLeft.empty())
     {
-        cout << "TEMP AND size of temp " << temp.size() << " ==> ";
-        for (size_t i = 0; i < temp.size(); i++)
+        int currentNumber = chargersLeft.top();
+        chargersLeft.pop();
+
+        if (currentNumber == maxNumber)
         {
-            cout << temp[i] << ", ";
-        }
-        cout << endl;
-
-        cout << "SAVE AND size of temp " << save.size() << " ==> ";
-        for (size_t i = 0; i < save.size(); i++)
-        {
-            cout << save[i] << ", ";
-        }
-        cout << endl;
-
-        currentNumber = temp[0];
-
-        cout << "current number is " << currentNumber << endl;
-
-        // can continue iterating
-        temp.erase(temp.begin());
-
-        if (currentNumber + 1 <= maxNumber)
-        {
-            //save.pop_back();
-
-            cout << "Index found " << globalIndex << endl;
-           
-            // found following number index
-            int followingNumberIndex = 0;
-            for (size_t i = 0; i < followingNumbers.size(); i++)
-            {
-                if (followingNumbers[i] == currentNumber)
-                {
-                    followingNumberIndex = i;
-                }
-            }
-
-            if (followingNumberIndex + 1 < followingNumbers.size())
-            {
-                temp.push_back(followingNumbers[followingNumberIndex + 1]);
-                save.push_back(followingNumbers[followingNumberIndex + 1]);
-            }
-            
-            if (followingNumberIndex + 2 < followingNumbers.size())
-            {
-                temp.push_back(followingNumbers[followingNumberIndex + 2]);
-                save.push_back(followingNumbers[followingNumberIndex + 2]);
-            }
-
-            if (followingNumberIndex + 3 < followingNumbers.size())
-            {
-                temp.push_back(followingNumbers[followingNumberIndex + 3]);
-                save.push_back(followingNumbers[followingNumberIndex + 3]);
-            }
+            nbDistinctArrangementsForSegment++;
         }
         else
         {
-            cout << "score !" << endl;
-            //temp.pop_back();
-            result++;
-            //save.push_back(currentNumber);
+            // find the index of the current number to add his 
+            int followingNumberIndex = 0;
+            for (size_t i = 0; i < arrangementSegment.size(); i++)
+            {
+                if (arrangementSegment[i] == currentNumber)
+                {
+                    followingNumberIndex = i;
+                    break;
+                }
+            }
+
+            for (size_t offset = minVoltageDiff; offset <= maxVoltageDiff; offset++)
+            {
+                if (followingNumberIndex + offset < arrangementSegment.size())
+                {
+                    chargersLeft.push(arrangementSegment[followingNumberIndex + offset]);
+                }
+            }
         }
-       /* else if (currentNumber - 1 > minNumber)
-        {
-            temp.pop_back();
-            temp.push_back(followingNumbers[i + 1]);
-        }*/
     }
 
-
-    for (size_t i = 0; i < temp.size(); i++)
-    {
-        cout << temp[i] << endl;
-    }
-    cout << "end" << endl;
-    cout << "Length of vector is " << temp.size() << " result is " << result << endl;
-    cout << "Test with save " << save.size() << endl;
-    return result;
+    return nbDistinctArrangementsForSegment;
 }
 
-
-int main()
+vector<int> RetrieveAdapterVoltageFromFile(string fileName)
 {
-    vector<int> adapters = vector<int>();
+    vector<int> adapterVotages = vector<int>();
 
-    int nbOneJoltDiff = 0;
-    int nbTwoJoltDiff = 0;
-    int nbThreeJoltsDiff = 0;
-
-    long long resultPart2 = 1;
-
-    string line;
-    ifstream  myfile;
-
-    myfile.open("input.txt");
+    ifstream myfile;
+    myfile.open(fileName);
     if (myfile.is_open())
     {
+        string line;
         while (getline(myfile, line))
         {
-            cout << line.c_str() << endl;
-            adapters.push_back(atoi(line.c_str()));
+            adapterVotages.push_back(atoi(line.c_str()));
         }
     }
     myfile.close();
 
+    return adapterVotages;
+}
+
+int main()
+{
+    cout << "Day 10 - Adapter Array" << endl;
+    vector<int> adapters = RetrieveAdapterVoltageFromFile("input.txt");
+    map<int, int> numberOfSameJoltDiff = map<int, int>
+    {
+        make_pair(1,0),
+        make_pair(2,0),
+        make_pair(3,0),
+    };
+
+    // Add the charing outlet voltage
     adapters.push_back(0);
 
     sort(adapters.begin(), adapters.end());
 
-    vector<int> numbers = vector<int>();
-
-    int currentDiffPath = 1;
-    int currentNumber = 0;
+    long long numberOfDistinctCombinaisons = 1;
+    vector<int> currentArrangement = vector<int>();
     for (int i = 0; i < adapters.size() - 1; i++)
     {
-        currentNumber = adapters[i];
-        int nextNumber = adapters[i+1];
+        int currentNumber = adapters[i];
 
-        /*int secondNextNumber = 0;
-        if (i - 2 >= 0)
+        int currentJoltDiff = adapters[i + 1] - currentNumber;
+        numberOfSameJoltDiff[currentJoltDiff] += 1;
+
+        // if the currentJoltDiff is three, we're guaranteed that all arrangements will have it.
+        // We can study how many possibilities will generate the segment of arrangement we gather.
+        currentArrangement.push_back(currentNumber);
+        if (currentJoltDiff == 3)
         {
-            secondNextNumber = adapters[i-2];
+            // The segment arrangement can be found "N" times where N is the result of the function.
+            numberOfDistinctCombinaisons *= ComputeFollowingGroup(currentArrangement, currentNumber);
+            currentArrangement.clear();
         }
-
-        int thirdNextNumber = 0;
-        if (i - 3 >= 0)
-        {
-            thirdNextNumber = adapters[i-3];
-        }*/
-        cout << "end" << endl;
-
-        int joltDiff = nextNumber - currentNumber;
-        nbOneJoltDiff = joltDiff == 1 ? nbOneJoltDiff + 1 : nbOneJoltDiff;
-        nbTwoJoltDiff = joltDiff == 2 ? nbTwoJoltDiff + 1 : nbTwoJoltDiff;
-        nbThreeJoltsDiff = joltDiff == 3 ? nbThreeJoltsDiff + 1 : nbThreeJoltsDiff;
-
-        cout << joltDiff << endl;
-        if (joltDiff == 3)
-        {
-            cout << "Push last number " << currentNumber << endl;
-            numbers.push_back(currentNumber);
-
-            int combinaisons = ComputeFollowingGroup(numbers, currentNumber);
-
-            cout << "Break ! Result is " << combinaisons << endl;
-            resultPart2 *= combinaisons;
-
-            numbers.clear();
-        }
-        else if (joltDiff == 1)
-        {
-            cout << "Push number " << currentNumber << endl;
-            numbers.push_back(currentNumber);
-
-            /*int currentSum = 0;
-            cout << "Start ! because " << joltDiff << endl;
-            if (i-2 >=0 && (currentNumber - secondNextNumber) == 2)
-            {
-                currentSum++;
-            }
-            if (i-3 >= 0 && (currentNumber - thirdNextNumber) == 3)
-            {
-                currentSum+=2;
-            }
-            cout << "sum is " << currentSum << endl;
-
-            if (currentSum > 0)
-            {
-                currentDiffPath *= currentSum;
-            }*/
-        }
-
-        cout << "end" << endl;
     }
 
-    if (numbers.size() > 0)
-    {
-        currentNumber = adapters[adapters.size() - 1];
-        cout << "Push last number " << currentNumber << endl;
-        numbers.push_back(currentNumber);
-        int combinaisons = ComputeFollowingGroup(numbers, currentNumber);
-
-        cout << "Break ! Result is " << combinaisons << endl;
-        resultPart2 *= combinaisons;
-    }
+    // Apply to last arrangement
+    int lastNumber = adapters[adapters.size() - 1];
+    currentArrangement.push_back(lastNumber);
+    numberOfDistinctCombinaisons *= ComputeFollowingGroup(currentArrangement, lastNumber);
 
     // our device's built-in adapter is always 3 higher than the highest adapter
-    nbThreeJoltsDiff++;
+    numberOfSameJoltDiff[3]++;
 
-    cout << "Nb two joft " << nbTwoJoltDiff << endl;
-
-    cout << "Result for part1 is : " << nbOneJoltDiff * nbThreeJoltsDiff << endl;
-    cout << "Result for part2 is : " << resultPart2 << endl;
+    cout << "Result for part 1 is : " << numberOfSameJoltDiff[1] * numberOfSameJoltDiff[3] << endl;
+    cout << "Result for part 2 is : " << numberOfDistinctCombinaisons << endl;
 }
