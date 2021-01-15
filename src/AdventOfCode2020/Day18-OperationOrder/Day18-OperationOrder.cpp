fc37458177659ceb25de9bd50a14fdef5e339a1f
Gregoire Boiron <gregoire.boiron@gmail.com>
2021-01-15 22:23:45 +0100

    = Clean day 18 plus add back part 1 result

--- src/AdventOfCode2020/Day18-OperationOrder/Day18-OperationOrder.cpp
+++ src/AdventOfCode2020/Day18-OperationOrder/Day18-OperationOrder.cpp
@@ -12,13 +12,11 @@ enum CurrentOperation { NONE, ADD, MULT };
 unsigned long long EvaluateExpression(std::string expression, int *offset)
 {
     unsigned long long result = 0;
-    //unsigned long long previousNumber = 0;
     unsigned long long subCalculation = 0;
     CurrentOperation ope = CurrentOperation::NONE;
 
     for (int i = *offset; i < expression.size(); i++)
     {
-        //std::cout << expression[i] << std::endl;
         if (expression[i] != ' ')
         {
             if (expression[i] == '+')
@@ -33,24 +31,19 @@ unsigned long long EvaluateExpression(std::string expression, int *offset)
             {
                 *offset = i + 1;
                 subCalculation = EvaluateExpression(expression, offset);
-                //std::cout << "subCalculation" << subCalculation << std::endl;
 
                 if (ope == CurrentOperation::NONE)
                 {
                     result = subCalculation;
-                    //std::cout << "NONE" << result << std::endl;
                 }
                 else if (ope == CurrentOperation::ADD)
                 {
                     result += subCalculation;
-                    //std::cout << "ADD" << result << std::endl;
                 }
                 else if (ope == CurrentOperation::MULT)
                 {
                     result *= subCalculation;
-                    //std::cout << "MULT" << result << std::endl;
                 }
-                //previousNumber = subCalculation;
 
                 i = *offset;
             }
@@ -67,19 +60,15 @@ unsigned long long EvaluateExpression(std::string expression, int *offset)
                 if (ope == CurrentOperation::NONE)
                 {
                     result = currentNumber;
-                    //std::cout << "NONE" << result << std::endl;
                 }
                 else if (ope == CurrentOperation::ADD)
                 {
                     result += currentNumber;
-                    //std::cout << "ADD" << result << std::endl;
                 }
                 else if (ope == CurrentOperation::MULT)
                 {
                     result *= currentNumber;
-                    //std::cout << "MULT" << result << std::endl;
                 }
-                //previousNumber = currentNumber;
             }
         }
     }
@@ -168,59 +157,14 @@ std::string AddParenthesis(std::string expression, char ope)
                     expression.insert(i + 1, ")");
                 }
             }
-
-            //std::cout << "index of i " << i << std::endl;
-/* for (auto it = indexes.begin(); it != indexes.end(); ++it)
-     std::cout << it->first << " => " << it->second << '\n';*/
         }
     }
 
     return expression;
 }
 
-unsigned long long EvaluateExpressionPart2(std::string expression, int* offset)
-{
-    // Apply parenthesis to prioritise additions
-    expression = AddParenthesis(expression, '+');
-
-    return EvaluateExpressionPart2(expression, offset);
-}
-
-// 145610164171889 => too high
-// 140338487202656 => too low
-
-
-void Part1(std::string fileName)
-{
-    std::ifstream file;
-    file.open(fileName);
-
-    std::string line;
-    if (file.is_open())
-    {
-        unsigned long long part1Result = 0;
-        int* offset = new int(0);
-        while (getline(file, line))
-        {
-            line = AddParenthesis(line, '+');
-            std::cout << line << std::endl;
-
-            *offset = 0;
-            unsigned long long currentResult = EvaluateExpression(line, offset);
-            //std::cout << "Current Result is " << currentResult << std::endl;
-
-            part1Result += currentResult;
-
-            //std::cout << "Cumulated result is " << part1Result << std::endl;
-        }
-
-        std::cout << "Result for part 1 is " << part1Result << std::endl;
-    }
-}
-
 void Tests()
 {
-    Part1("example.txt");
     int* offset = new int(0);
     std::cout << (EvaluateExpression("1 + 2 * 3 + 4 * 5 + 6", offset) == 71 ? "true" : "false") << std::endl;
     *offset = 0;
@@ -235,7 +179,6 @@ void Tests()
     std::cout << (EvaluateExpression("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2", offset) == 13632 ? "true" : "false") << std::endl;
 
     // My examples
-
     *offset = 0;
     std::cout << (EvaluateExpression("((1 + 2) * (3 + (4 * (5 + 6))))", offset) == 141 ? "true" : "false") << std::endl;
     *offset = 0;
@@ -250,7 +193,7 @@ void Tests()
     std::cout << (EvaluateExpression("(5 * (3 * 2 * 2 + 4 + 4) + 6 * (8 + 6 * 3 + 6)) * (3 * 7 * (5 * 9 * 7 + 5 * 6 * 2) + 6) + 2 + 8 * ((9 + 5 + 4 + 8 * 5 * 4) + 3) * 5", offset) == 114 ? "true" : "false") << std::endl;
 }
 
-void TestPart2()
+void TestsPart2()
 {
     //std::cout << AddParenthesis("4 * 2 + 2", '+') << std::endl;
     //std::cout << AddParenthesis("4 * (2 + 2)", '+') << std::endl;
@@ -286,8 +229,33 @@ void TestPart2()
 
 int main()
 {
-    std::cout << "Operation Order" << std::endl;
+    std::cout << "Day 18 - Operation Order" << std::endl;
 
-    //TestPart2();
-    Part1("input.txt");
+    std::ifstream file;
+    file.open("input.txt");
+
+    if (file.is_open())
+    {
+        unsigned long long part1Result = 0;
+        unsigned long long part2Result = 0;
+        int* offset = new int(0);
+
+        std::string line;
+        while (getline(file, line))
+        {
+            // Part 1
+            *offset = 0;
+            unsigned long long currentResult = EvaluateExpression(line, offset);
+            part1Result += currentResult;
+
+            // Part 2
+            line = AddParenthesis(line, '+');
+            *offset = 0;
+            currentResult = EvaluateExpression(line, offset);
+            part2Result += currentResult;
+        }
+
+        std::cout << "Result for part 1 is " << part1Result << std::endl;
+        std::cout << "Result for part 2 is " << part2Result << std::endl;
+    }
 }
